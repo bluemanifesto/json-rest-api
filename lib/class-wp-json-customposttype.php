5d57d3f41a8fed3031a7eebce6cb16c2816c6062
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-21 12:37:01 +0100

    add some debugging

--- lib/class-wp-json-customposttype.php
+++ lib/class-wp-json-customposttype.php
@@ -196,6 +196,8 @@ abstract class WP_JSON_CustomPostType extends WP_JSON_Posts {
 		$_post = parent::prepare_post( $post, $context );
 
 		// Override entity meta keys with the correct links
+    error_log($post['ID'], 0);
+    error_log($post['post_author'], 0);
 		$_post['meta'] = array(
 			'links' => array(
 				'self'            => json_url( $this->base . '/' . $post['ID'] ),
